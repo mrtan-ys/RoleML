070960210314124a9b75efd552d2d19f77c71148
mrtan-ys <seystan@mail.scut.edu.cn>
2026-08-02 18:14:22 +0800

    feat(example): add MobileNetV3 & ImageNet-100 (CMC) adaptation files (code and configurations) for FL

---
+++ examples/federated_learning/src/federated_learning/workload/imagenet_100.py
@@ -0,0 +1,5 @@
+from roleml.library.workload.datasets.zoo.image.imagenet_100.impl import ImageNet100TestDataset, ImageNet100TrainDataset
+
+
+MyImageNet100TrainDataset = ImageNet100TrainDataset
+MyImageNet100TestDataset = ImageNet100TestDataset
---
+++ examples/federated_learning/src/federated_learning/workload/mobilenet_v3.py
@@ -0,0 +1,5 @@
+from roleml.library.workload.models.zoo.torch.cnn.mobilenet_v3 import MobileNetV3SmallModel
+
+
+class MyMobileNetV3SmallModel(MobileNetV3SmallModel):
+    pass
